d99d3e0ff4503caa435960d5a7f1b8ed4839e555
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-06-19 12:38:30 -0300

    Punto 1

---
+++ ConsoleApp1/ConsoleApp1/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Helpers
+{
+    static public class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion(string ruta, string NombreArchivo)
+        {
+            string Extencion = ".dat";
+
+            if (!File.Exists(ruta + NombreArchivo + Extencion))
+            {
+                File.Create(ruta + NombreArchivo + Extencion);
+            }
+        }
+
+        public static string LeerConfiguracion(string ruta, string NombreArchivo)
+        {
+            string Extencion = ".dat";
+            if (!File.Exists(ruta + NombreArchivo + Extencion))
+            {
+                return File.ReadAllText(ruta + NombreArchivo + Extencion);
+            }
+
+            return "Vacio o no existe";
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Helpers;
+
+namespace ConsoleApp1
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            string path = @"bin\debug" ;
+            string txt = "*.txt";
+            string mp3 = "*.mp3";
+
+            string[] dirtxt = Directory.GetDirectories(path, txt);
+            string[] dirmp3 = Directory.GetDirectories(path, mp3);
+
+            string NuevaRuta = "Alguna nueva ruta";
+
+            for (int i = 0; i < dirtxt.Length; i++)
+            {
+                File.Move(dirtxt[i],NuevaRuta);
+            }
+
+            for (int i = 0; i < dirmp3.Length; i++)
+            {
+                File.Move(dirmp3[i], NuevaRuta);
+            }
+        }
+    }
+}
